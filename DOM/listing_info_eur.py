54302f76f34f9fb8ccf5f0308dee39a91bbeb063
NikiAY <121106574+NikiAY@users.noreply.github.com>
2023-01-25 16:39:08 +0400

    Create listing_info_eur.py

---
+++ DOM/listing_info_eur.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+from pydantic import BaseModel
+
+class listing_info_eur(BaseModel):
+    price: int
+    price_change_count:int
+    area:int
+    month_published:int
+    year_published:int
+    type_group_id:int
+    city_id : int
+    has_lift:bool
+    has_parking_space:bool
+    IT:int
+    PT:int
+    SP:int
